21135e4e55d2384e3f92e423e73093b4a5521119
Max Silva <maxsilva@MacBook-Pro-de-Max.local>
2023-11-10 20:42:19 -0300

    feat(calculator): added calculator router

--- src/express/app/controllers/instance/instance.controller.ts
+++ src/express/app/controllers/instance/instance.controller.ts
@@ -6,76 +6,121 @@ import { IUserInfoController } from "../../../../models/express/user-info/user-i
 import { tmbStopMonitoring } from "../../../../actions/tmb-stop-monitoring/tmb-stop-monitoring";
 
 class InstanceController {
-  getList(req: Request, res: Response): Response<IInstanceList> {
-    try {
-      const instances = userMonitors.map((i: TMBCore, index: number) => {
-        return {
-          id: index,
-          user: i.username
+    getList(req: Request, res: Response): Response<IInstanceList> {
+        try {
+            const instances = userMonitors.map((i: TMBCore, index: number) => {
+                return {
+                    id: index,
+                    user: i.username,
+                };
+            });
+
+            return res.json({
+                data: instances,
+            });
+        } catch (err) {
+            return res.sendStatus(500);
+        }
+    }
+
+    async getEnrouteList(req: Request, res: Response) {
+        try {
+            const id = Number(req.params.id);
+            const userInfo = await userMonitors[id].userInfo();
+            const trainInfo = await userMonitors[id].trainInfo(userInfo!);
+            res.json({
+                data: trainInfo,
+            });
+        } catch (err) {
+            return res.sendStatus(500);
+        }
+    }
+
+    async getUserInfo(
+        req: Request,
+        res: Response
+    ): Promise<Response<IUserInfoController | null>> {
+        try {
+            const id = Number(req.params.id);
+            const userInfo = await userMonitors[id].userInfo();
+            return res.json({
+                data: {
+                    companyName: userInfo?.userData.company,
+                    companyValue: userInfo?.userData.stock,
+                    money: userInfo?.userData.account,
+                    points: userInfo?.userData.points,
+                    energy: userInfo?.userData.spotPct,
+                    fuel: userInfo?.userData.fuelPct,
+                    parkedTrains: userInfo?.userData.station,
+                    enrouteTrains: userInfo?.userData.enroute,
+                    pendingTrains: userInfo?.userData.pending,
+                    realism: userInfo?.userData.realism === 1 ? true : false,
+                },
+            });
+        } catch (err) {
+            return res.sendStatus(500);
+        }
+    }
+
+    getLogs(req: Request, res: Response) {
+        const id = Number(req.params.id);
+        return res.json({ data: userMonitors[id].showLogs("object") });
+    }
+
+    calculatorRoute(req: Request, res: Response) {
+        const capacity = Number(req.params.capacity);
+        const passenger = Number(req.params.passenger);
+        const [hours, minutes, seconds] = req.params.time
+            .split(":")
+            .map(Number);
+
+        // Dados de entrada
+        const tempoViagemMinutos: number = hours * 60 + minutes + seconds; // horas * 60min + min + seg;
+        const limiteHorasMin: number = 24 * 60; // 24 horas em minutos
+        const limiteHoras: number = 24;
+
+        // Cálculo
+        let tempoTotalMinutos: number = 0;
+        let viagensFeitas: number = 0;
+
+        while (tempoTotalMinutos <= limiteHorasMin) {
+            tempoTotalMinutos += tempoViagemMinutos;
+            if (tempoTotalMinutos <= limiteHorasMin) {
+                viagensFeitas++;
+            }
+        }
+
+        const viagensDiarias: number = limiteHoras / (tempoViagemMinutos / 60);
+        const passageirosTransportados: number = viagensFeitas * capacity;
+        const excedeuLimite: string =
+            passageirosTransportados > passenger ? "Sim" : "Não";
+        const trensNecessarios: number = Math.floor(
+            passenger / (capacity * viagensDiarias)
+        );
+
+        // Objeto com as informações
+        const informacoes: Record<string, any> = {
+            viagensFeitas: viagensFeitas,
+            passageirosTransportados: passageirosTransportados,
+            excedeuLimite: excedeuLimite,
+            trensNecessarios: trensNecessarios,
         };
-      });
 
-      return res.json({
-        data: instances
-      });
-    } catch (err) {
-      return res.sendStatus(500);
+        return res.json({ data: informacoes });
     }
-  }
 
-  async getEnrouteList(req: Request, res: Response) {
-    try {
-      const id = Number(req.params.id);
-      const userInfo = await userMonitors[id].userInfo();
-      const trainInfo = await userMonitors[id].trainInfo(userInfo!);
-      res.json({
-        data: trainInfo
-      })
-    } catch (err) {
-      return res.sendStatus(500);
-    }
-  }
-
-  async getUserInfo(req: Request, res: Response): Promise<Response<IUserInfoController | null>> {
-    try {
-      const id = Number(req.params.id);
-      const userInfo = await userMonitors[id].userInfo()
-      return res.json({
-        data: {
-          companyName: userInfo?.userData.company,
-          companyValue: userInfo?.userData.stock,
-          money: userInfo?.userData.account,
-          points: userInfo?.userData.points,
-          energy: userInfo?.userData.spotPct,
-          fuel: userInfo?.userData.fuelPct,
-          parkedTrains: userInfo?.userData.station,
-          enrouteTrains: userInfo?.userData.enroute,
-          pendingTrains: userInfo?.userData.pending,
-          realism: userInfo?.userData.realism === 1 ? true : false,
+    async stopMonitoring(req: Request, res: Response) {
+        const id = Number(req.params.id);
+        if (await tmbStopMonitoring(userMonitors[id])) {
+            return res.json({
+                data: {
+                    stopped: true,
+                    message: "A instância foi finalizada.",
+                },
+            });
         }
-      });
-    } catch (err) {
-      return res.sendStatus(500)
+        return res.sendStatus(500);
     }
-  }
-
-  getLogs(req: Request, res: Response) {
-    const id = Number(req.params.id);
-    return res.json({ data: userMonitors[id].showLogs('object') });
-  }
-
-  async stopMonitoring(req: Request, res: Response) {
-    const id = Number(req.params.id);
-    if(await tmbStopMonitoring(userMonitors[id])) {
-      return res.json({
-        data: {
-          stopped: true,
-          message: 'A instância foi finalizada.'
-        }
-      })
-    }    
-    return res.sendStatus(500);    
-  }
 }
 
 const instanceController = new InstanceController();
